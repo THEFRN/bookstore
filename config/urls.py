d5e6523f39a1a3bfb9ab9c2f3cffd49957bf2f7d
THEFRN <farhangian.ahmad@gmail.com>
2023-05-31 10:56:38 +0330

    created signup view

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accoutns/', include('django.contrib.auth.urls')),
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 ]
