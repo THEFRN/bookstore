86b95c710aa22ad6e661f511aa3883c49d423975
THEFRN <farhangian.ahmad@gmail.com>
2023-06-01 15:40:39 +0330

    add tests for accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from  django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpPageTests(TestCase):
+    username = 'my_username'
+    email = 'my@gmail.com'
+
+    def test_sign_up_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_up_page_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_sign_up_page_template_usage(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_sign_up_page_content(self):
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'Sign Up')
+
+    def test_sign_up_form(self):
+        user = get_user_model().objects.create_user(
+            self.username,
+            self.email,
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
