b6f69c626e6d9eb0f8de0a994528bac27f8ca961
THEFRN <farhangian.ahmad@gmail.com>
2023-06-02 13:40:20 +0330

    test are ok

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from  django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 
 
 class SignUpPageTests(TestCase):
