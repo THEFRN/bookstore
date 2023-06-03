7d33f6785b4505a797156911578552674e569139
THEFRN <farhangian.ahmad@gmail.com>
2023-06-03 17:31:30 +0430

    create detail view for books

--- books/views.py
+++ books/views.py
@@ -7,3 +7,9 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    template_name = 'books/book_detail.html'
+    model = Book
+    context_object_name = 'book'
