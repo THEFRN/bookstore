f430878cfe15b81529fef4d72427d60b0ab5cf6f
THEFRN <farhangian.ahmad@gmail.com>
2023-06-07 16:07:27 +0430

    add related to Comments.book

--- books/models.py
+++ books/models.py
@@ -19,7 +19,7 @@ class Book(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
     date_time_created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
 
